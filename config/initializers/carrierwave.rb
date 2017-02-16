39de6488c78adeb17968d30eb12c0979dc5dff2a
shaheming <hemingsha@gmail.com>
2017-02-16 22:38:28 +0800

    carrier.wave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,21 +1,17 @@
-require 'carrierwave/storage/abstract'
+
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
- if Rails.env.production?
-    	config.storage :fog
-	# config.fog_provider = 'fog/aws'
-	config.fog_credentials={
-		provider: "AWS",
-		aws_access_key_id: ENV["AKIAJCLVC5FFFLDXMTHQ"],
-
-		aws_secret_access_key: ENV["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"],
-
-		region: 'ap-northeast-1'
+ if Rails.env.production? 
+ 	config.storage :fog
+	 config.fog_credentials = {
+		 provider:              'AWS',  
+		 aws_access_key_id:       ENV["AWS_ACCESS_KEY_ID"],
+		 aws_secret_access_key:   ENV["AWS_SECRET_ACCESS_KEY"],
+		 region:  				  ENV["AWS_REGION"]    
 
 	} 	
-
-	config.fog_directory = ENV["fullstack-sand"]
+	config.fog_directory = ENV["AWS_BUCKET_NAME"]
  else
  	config.storage :file
  end
