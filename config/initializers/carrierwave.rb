dc72726a013e1bd4a6d6602b34bb11dfbb6c57c6
shaheming <hemingsha@gmail.com>
2017-02-16 09:55:10 +0800

    try fo fix bug

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
 CarrierWave.configure do |config|
  if Rails.env.production?
-    config.storage :fog
+    
 	# config.fog_provider = 'fog/aws'
 	config.fog_credentials={
 		provider: "AWS",
@@ -11,7 +11,7 @@ CarrierWave.configure do |config|
 		region: 'ap-northeast-1'
 
 	} 	
-
+config.storage :fog
 	config.fog_directory = ENV["fullstack-sand"]
  else
  	config.storage :file
