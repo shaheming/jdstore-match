877e4dd3036ce1f98fee8cc769f71854f77948d9
shaheming <hemingsha@gmail.com>
2017-02-17 12:03:05 +0800

    add aasm_state to order

--- Gemfile
+++ Gemfile
@@ -42,7 +42,7 @@ gem 'mini_magick'
 gem 'figaro'
 gem 'fog-aws'
 gem 'letter_opener',group: :development
-
+gem 'aasm'
 
 group :production do
  gem 'pg'
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -32,14 +32,16 @@ class OrdersController < ApplicationController
 	def pay_with_alipay
 		@order = Order.find_by_token(params[:id])
 		@order.set_payment_with!("alipay")
-		@order.pay!
+		@order.make_payment!
+
 		redirect_to order_path(@order.token),notice:"pay with alipay!"
 	end
 
 	def pay_with_wechat
 		@order = Order.find_by_token(params[:id])
 		@order.set_payment_with!("wechat")
-		@order.pay!
+		@order.make_payment!
+
 		redirect_to order_path(@order.token),notice:"pay with wechat!"
 	end
 
