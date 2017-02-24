521f774ee5153aeb47f40dcfd74968eef349e8a5
shaheming <hemingsha@gmail.com>
2017-02-24 22:11:13 +0800

    delete to migration

--- db/migrate/20170224013549_add_time_to_product.rb
+++
@@ -1,4 +0,0 @@
-class AddTimeToProduct < ActiveRecord::Migration[5.0]
-  def change
-  end
-end
--- db/migrate/20170224015826_remove_time_column_in_product.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTimeColumnInProduct < ActiveRecord::Migration[5.0]
-  def change
-  	remove_column :products,:time
-  end
-end
