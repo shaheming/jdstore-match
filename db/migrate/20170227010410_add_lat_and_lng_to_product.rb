03877c5fbda65c370ffe40ea28ae9def255ee355
shaheming <hemingsha@gmail.com>
2017-02-27 11:10:26 +0800

    fix the decimal overflow

--- db/migrate/20170227010410_add_lat_and_lng_to_product.rb
+++ db/migrate/20170227010410_add_lat_and_lng_to_product.rb
@@ -1,6 +1,6 @@
 class AddLatAndLngToProduct < ActiveRecord::Migration[5.0]
   def change
-  	add_column :products,:lng,:decimal, :precision => 15, :scale => 13,default: 0
-  	add_column :products,:lat,:decimal, :precision => 15, :scale => 13,default: 0
+  	add_column :products,:lng,:decimal, :precision => 10, :scale => 6,default: 0
+  	add_column :products,:lat,:decimal, :precision => 10, :scale => 6,default: 0
 end
 end
