e381d5a85f8828387d6edf7699d7bb4fd17c3217
shaheming <hemingsha@gmail.com>
2017-02-27 12:35:38 +0800

    fix  the migrate bug

--- db/migrate/20170227035956_add_lat_and_lng_to_product.rb
+++ db/migrate/20170227035956_add_lat_and_lng_to_product.rb
@@ -1,7 +1,5 @@
 class AddLatAndLngToProduct < ActiveRecord::Migration[5.0]
   def change
-  	remove_column :products,:lng
-  	remove_column :products,:lat
   	add_column :products,:lng,:string,default: "0"
   	add_column :products,:lat,:string,default: "0"
   end
