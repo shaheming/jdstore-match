042cb190181139c324f90dc6ec35dfc7f3ffa529
shaheming <hemingsha@gmail.com>
2017-03-02 10:09:19 +0800

    remove the pay flash

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -31,10 +31,6 @@ class OrdersController < ApplicationController
 		@product_lists = @order.product_lists
 		if @order.is_paid?
 			@product_lists = @order.product_lists
-			
-			# str_test ="the loar of the ring"+" " +"2007-12-12 17:20"+" " \
-			#           +"col: 7"+" "+ "raw: 7" +"\ "+" aad  ddddddddda ddddddddd"
-			# @qr = RQRCode::QRCode.new( str_test, :size => 4, :level => :l )
 		end
 	end
 
@@ -42,21 +38,18 @@ class OrdersController < ApplicationController
 		@order = Order.find_by_token(params[:id])
 		@order.set_payment_with!("alipay")
 		@order.make_payment!
-
-		redirect_to order_path(@order.token),notice:"pay with alipay!"
+		redirect_to order_path(@order.token)
 	end
 
 	def pay_with_wechat
 		@order = Order.find_by_token(params[:id])
 		@order.set_payment_with!("wechat")
 		@order.make_payment!
-
-		redirect_to order_path(@order.token),notice:"pay with wechat!"
+		redirect_to order_path(@order.token)
 	end
 	def apply_to_cancel
 		@order = Order.find(params[:id])
 		OrderMailer.apply_cancel(@order).deliver!
-		# flash[:notice] = "Application Submitted"
 		redirect_to :back
 	end
 	def generate_tickets
