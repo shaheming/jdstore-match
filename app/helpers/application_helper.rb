19d121a43b0f4d8d749a872101a05582e0277b60
shaheming <hemingsha@gmail.com>
2017-03-14 18:41:34 +0800

    rm redundent code when render image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,55 +1,28 @@
 module ApplicationHelper
 	
-def show_product_image(product)
+def big_image(product,img_id)
     link_to product_path(product) do
     if product.image.present?
-      image_tag(product.image.thumb.url,clas:"product-img")
+      image_tag(product.image.thumb.url,id:img_id)
     else
-      image_tag("http://placehold.it/200x296&text=No Pic",class:"product-img")
+      image_tag("http://placehold.it/200x296&text=No Pic",id:img_id)
     end
   end
 end
 
-def render_product_image_search(product)
+
+###
+
+def small_image(product,img_id)
     link_to product_path(product) do
     if product.image.present?
-      image_tag(product.image.thumb.url,id:"search-img")
+      image_tag(product.image.small.url, id:img_id)
     else
-      image_tag("http://placehold.it/200x296&text=No Pic",id:"search-img")
+      image_tag("http://placehold.it/100x149&text=No Pic", id: img_id)
     end
   end
 end
 
-def show_cart_items_image(product)
-    link_to product_path(product) do
-    if product.image.present?
-      image_tag(product.image.small.url, id: "cart-items-img")
-    else
-      image_tag("http://placehold.it/100x149&text=No Pic", id: "cart-items-img")
-    end
-  end
-end
-
-def render_landingpage_image(product)
-
-    if product.image.present?
-      image_tag(product.image.small.url, id: "landingpage-img")
-    else
-      image_tag("http://placehold.it/100x149&text=No Pic", id: "landingpage-img")
-    end
-
-end
-
-
-def render_samll_img(product)
-    link_to product_path(product) do
-    if product.image.present?
-      image_tag(product.image.small.url)
-    else
-      image_tag("http://placehold.it/100x149&text=No Pic")
-    end
-  end
-end
 
 def render_highlight_content(product_text,query_string)
     excerpt_cont = excerpt(product_text, query_string, radius: 500)
