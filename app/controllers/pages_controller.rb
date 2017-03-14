cd8bf6a9a01674b39e5eb9013dab050247995708
shaheming <hemingsha@gmail.com>
2017-03-14 19:10:43 +0800

    improve some detail

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,21 +4,6 @@ class PagesController < ApplicationController
 	end
 
 	def test
-		@products = Product.all
-		p= Product.first
-			  	l = "50.111111".to_f
-		  	ln = "50.1111111".to_f
-		  @hash = Gmaps4rails.build_markers(p) do |p, marker|
-	
-	      marker.lat l
-	      marker.lng ln
-	      marker.title p.location
-	       marker.picture({
-              :url => "/assets/images/location.png",
-              :width   => 20,
-              :height  => 35
-      }) 
-    	end 
 	end
 	def test_test
 		@products = Product.all
